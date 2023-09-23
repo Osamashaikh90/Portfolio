6d507a1286fe5ca211aad222e45fdd4b8747797a
Osama <shaikhosama499@gmail.com>
2023-09-23 07:03:43 +0530

    feat(app): writer helper created for type writer effect

---
+++ src/app/components/Writer.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Typewriter from "typewriter-effect";
+const Writer:any = () => {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Full Stack Web Developer",
+          "NextJs Developer",
+          "MERN Stack Developer",
+        ],
+              autoStart: true,
+              devMode: true,
+              skipAddStyles: false,
+        // pauseFor:'4',
+              loop: true,
+        delay:140,
+              deleteSpeed: 80,
+        
+      }}
+    />
+  )
+}
+
+export default Writer
