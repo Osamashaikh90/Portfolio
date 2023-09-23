471dcd639bdeb0cc822faaf16d17f683487ad4d6
Osama <shaikhosama499@gmail.com>
2023-09-23 07:04:33 +0530

    feat(app): project couter component created for dyna,ically counting projects

---
+++ src/app/components/ProjectCounter.tsx
@@ -0,0 +1,19 @@
+// components/ProjectCounter.tsx
+import { useEffect, useState } from 'react';
+import { projectData } from '../utils/projectData';
+
+function ProjectCounter() {
+  const [projectCount, setProjectCount] = useState<number | null>(null);
+
+  useEffect(() => {
+    setProjectCount(projectData.length);
+  }, []);
+  // console.log(projectCount);
+  return (
+    <div>
+      {projectCount !== null ? projectCount : 'Loading...'}
+    </div>
+  );
+}
+
+export default ProjectCounter;
