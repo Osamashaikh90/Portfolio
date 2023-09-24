b059a3fd430c019b56c18114720084a51db810c8
Osama <shaikhosama499@gmail.com>
2023-09-25 01:39:20 +0530

    feat(app): work card created to display work experience

---
+++ src/app/components/WorkCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { workData, WorkDataTypes } from '../utils/work'
+import {PiCalendarBold} from "react-icons/pi"
+interface WorkCardProps {
+  work:WorkDataTypes ;
+}
+const EducationCard:React.FC<WorkCardProps> = ({work}) => {
+  return (
+    <>
+          <div className='grid-item ml-1'>
+      <span className="round inline-block absolute left-[48.7%] w-3 h-3 bg-[#ff5f9e] z-10  bg-opacity-100 rounded-[50%]"></span>
+              <h3 className='font-bold text-base font-Croissant '>{work.position}</h3>
+              <div className='flex flex-col gap-1'>
+              <span className='text-[#c4cddc] text-sm'> {work.company}</span>
+              <span className="text-[#818791]">{work.description}</span>
+                  <span className='text-[#7d8590] text-sm flex gap-[2px] items-center'><PiCalendarBold/>{work.date}</span>
+        </div>
+      </div>
+      </>
+  )
+}
+
+export default EducationCard
