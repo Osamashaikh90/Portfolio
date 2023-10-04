af9c378c305fc73b31015b1a37b7967cc5707bde
Osama <shaikhosama499@gmail.com>
2023-10-04 23:45:00 +0530

    chore(app): icons data added for fetching on skills page

---
+++ src/app/utils/icons.ts
@@ -0,0 +1,132 @@
+
+
+// import graphql from "../images/graphql.png";
+// import github from "../images/github.png";
+
+
+export interface IconsTypes  {
+  id: number;
+  src: string;
+    library: string;
+    style:string
+}
+
+
+export const iconsData: Array<IconsTypes> = [
+ {
+    "id": 1,
+    "src": "../images/html.png",
+    "library": "di",
+     "style": "shadow-orange-500",
+  },
+ {
+    "id": 2,
+    "src": "../images/css.png",
+     "library": "di",
+    "style":"shadow-blue-500"
+  },
+ 
+ {
+    "id": 3,
+    "src": "../images/javascript.png",
+     "library": "bi",
+    "style": "shadow-yellow-500",
+  },
+
+ {
+    "id": 4,
+    "src": "../images/react.png",
+     "library": "bi",
+    "style": "shadow-sky-600",
+  },
+ {
+    "id": 5,
+    "src": "../images/nextjs.png",
+     "library": "cg",
+    "style": "shadow-white",
+  },
+ {
+    "id": 6,
+    "src": "../images/tailwind.png",
+    "library": "cg",
+ "style": "shadow-cyan-600",
+  },
+ {
+    "id": 7,
+    "src":"../images/C.png",
+        "library": "cg",
+ "style": "shadow-blue-700",
+  },
+ {
+    "id": 8,
+    "src": "../images/C++.png",
+     "library": "cg",
+    "style": "shadow-sky-400",
+  },
+ {
+    "id": 9,
+    "src": "../images/node.png",
+     "library": "cg",
+    "style": "shadow-lime-500",
+  },
+ {
+    "id": 10,
+    "src": "../images/express.png",
+     "library": "cg",
+    "style": "shadow-gray-500 pt-10",
+  },
+ {
+    "id": 11,
+    "src": "../images/mongo.png",
+     "library": "cg",
+    "style": "shadow-lime-500",
+  },
+ {
+    "id": 12,
+    "src": "../images/Postgresql.png",
+     "library": "cg",
+    "style": "shadow-sky-400",
+  },
+
+
+]
+
+export const tools: Array<IconsTypes> = [
+ {
+    "id": 1,
+    "src": "../images/vs.png",
+    "library": "cg",
+"style":"shadow-blue-500"
+  },
+ {
+    "id": 2,
+     "src": "../images/git.png",
+    "library": "cg",
+"style":"shadow-orange-700"
+ 
+  },
+ {
+    "id": 3,
+      "src": "../images/github.png",
+    "library": "cg",
+"style":"shadow-white"
+  },
+ {
+    "id": 4,
+    "src": "../images/postman.webp",
+    "library": "cg",
+"style":"shadow-orange-500"
+  },
+ {
+    "id": 5,
+    "src": "../images/antd.png",
+    "library": "cg",
+   "style":"shadow-red-500 from-blue-500 to-red-500"
+  },
+ {
+    "id": 6,
+    "src": "../images/vercel.png",
+    "library": "cg",
+   "style":"shadow-gray-300 pt-10"
+  }
+]
