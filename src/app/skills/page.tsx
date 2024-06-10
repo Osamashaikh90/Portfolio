f347d36de2dd4956f1c1306cbde220b5baa1a6a8
Osama <shaikhosama499@gmail.com>
2024-06-10 19:00:55 +0530

    feat(app): glowing effect on cursor movement added

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,14 +1,23 @@
 "use client";
-import React, { useRef,useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Tooltip as ReactTooltip } from "react-tooltip";
 import { IconsTypes, iconsData, tools } from "../utils/icons"
 import ContactForm from '../components/ContactForm';
 import SocialLinks from '../components/SocialLinks';
 import GoToTop from '../components/GoToTop';
+import Tilt from "react-parallax-tilt";
 
-const Skills:React.FC = () => {
+const Skills: React.FC = () => {
   const contactFormRef = useRef<HTMLDivElement | null>(null);
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const card = e.currentTarget;
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
 
+    card.style.setProperty('--x', `${x}px`);
+    card.style.setProperty('--y', `${y}px`);
+  };
   useEffect(() => {
     if (window.scrollToContact) {
       contactFormRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -18,33 +27,36 @@ const Skills:React.FC = () => {
   return (
     <div className='text-white mx-4 lg:mx-[70px] xl:mx-32 mb-5  grid gap-5 midp:gap-10 md:gap-6 lg:gap-10 grid-cols-2 sm:grid-cols-3 midp:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 '>
       <div className='flex flex-col items-center justify-center pb-1 col-span-2 sm:col-span-3 midp:col-span-4 md:col-span-5 lg:col-span-6'><h2 className='text-lg items-center font-headings tracking-wide py-3'> Languages & Framworks 💻</h2>
-          <div className='w-[100%] h-[1px] bg-[#2b2e33] '></div></div>
+        <div className='w-[100%] h-[1px] bg-[#2b2e33] '></div></div>
       {iconsData.map((icon: IconsTypes) => (
         <>
-             <ReactTooltip
-        id={icon.content}
+          <ReactTooltip
+            id={icon.content}
             place="top"
             variant='light'
-        content={icon.content}
-      />
-            <div key={icon.id}  data-tooltip-id={icon.content} className={`cursor-pointer shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${icon.style}`}>
-              <img src={icon.src} alt="" className="w-20 mx-auto" />
+            content={icon.content}
+          />
+          <div key={icon.id} onMouseMove={handleMouseMove} data-tooltip-id={icon.content} className={`card cursor-pointer shadow-md hover:scale-105 duration-500 py-3 rounded-lg  ${icon.style}`}
+            style={{ '--clr': icon.bg } as React.CSSProperties}>
+            <img src={icon.src} alt="" className="w-20 mx-auto" />
 
           </div>
         </>))}
       <div className='flex flex-col items-center justify-center  pb-1 col-span-2 sm:col-span-3 midp:col-span-4 md:col-span-5 lg:col-span-6'><h2 className='text-lg items-center font-headings tracking-wide py-3'> Tools 🧑‍💻</h2>
-          <div className='w-[100%] h-[1px] bg-[#2b2e33] '></div></div>
+        <div className='w-[100%] h-[1px] bg-[#2b2e33] '></div></div>
       {tools.map((icon: IconsTypes) => (
-            <><ReactTooltip
-                id={icon.content}
+        <>
+        <ReactTooltip
+          id={icon.content}
           place="top"
           variant='light'
-                content={icon.content} />
-            <div  data-tooltip-id={icon.content} key={icon.id} className={`cursor-pointer shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${icon.style}`}>
-        <img src={icon.src} alt="" className="w-20 mx-auto"/>
+          content={icon.content} />
+          <div data-tooltip-id={icon.content} onMouseMove={handleMouseMove} key={icon.id} className={`card cursor-pointer shadow-md hover:scale-105 duration-500 py-3 rounded-lg ${icon.style}`}
+            style={{ '--clr': icon.bg } as React.CSSProperties}>
+            <img src={icon.src} alt="" className="w-20 mx-auto" />
           </div>
         </>))}
- <div className='flex flex-col items-center justify-center  pb-1 col-span-2 sm:col-span-3 midp:col-span-4 md:col-span-5 lg:col-span-6'><h2 className='text-lg items-center tracking-wide py-3 font-headings'>Take a Coffee & Chat with ME 🤓 </h2>
+      <div className='flex flex-col items-center justify-center  pb-1 col-span-2 sm:col-span-3 midp:col-span-4 md:col-span-5 lg:col-span-6'><h2 className='text-lg items-center tracking-wide py-3 font-headings'>Take a Coffee & Chat with ME 🤓 </h2>
         <div className='w-[100%] h-[1px] bg-[#2b2e33] '></div></div>
       <div ref={contactFormRef} className='border col-span-2 sm:col-span-3 midp:col-span-4 md:col-span-5 lg:col-span-6 border-[#3d3e41] rounded '>
         <ContactForm />
--- src/app/utils/icons.ts
+++ src/app/utils/icons.ts
@@ -3,6 +3,7 @@ export interface IconsTypes  {
   src: string;
     library: string;
    style: string;
+   bg:string
    content: string
 }
 
@@ -13,6 +14,7 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745240/Portfolio/images/hpecqqwli1r3uh7srubk.png",
      "library": "di",
     "style": "shadow-orange-500",
+    "bg":"rgba(255, 165, 0)",
     "content":"HTML"
   },
  {
@@ -20,6 +22,7 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745238/Portfolio/images/rh8s7bzgsa1yfkxae22d.png",
      "library": "di",
     "style": "shadow-blue-500",
+    "bg":"rgba(14, 165, 223) ",
     "content":"CSS"
   },
  
@@ -28,6 +31,7 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745240/Portfolio/images/eznfrnzutk7ojx2ssbhr.png",
      "library": "bi",
     "style": "shadow-yellow-500",
+    "bg":"rgb(234, 179, 8)",
     "content":"JavaScript"
   },
 
@@ -36,6 +40,7 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745242/Portfolio/images/nw1wbvodrnpyephdrxww.png",
      "library": "bi",
     "style": "shadow-sky-600",
+    "bg":"rgba(14, 165, 233) ",
     "content":"ReactJs"
   },
  {
@@ -43,6 +48,7 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745240/Portfolio/images/rzmp6bbrjvarmtq8s3na.png",
      "library": "cg",
     "style": "shadow-white",
+    "bg":"rgba(255, 255, 255, 0.9)",
     "content":"NextJs"
   },
  {
@@ -50,20 +56,23 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745242/Portfolio/images/kntuek1gzvg378fbhl8m.png",
     "library": "cg",
     "style": "shadow-cyan-600",
+    "bg":"rgb(0, 255, 213)",
  "content":"Tailwind"
   },
  {
     "id": 7,
     "src":"https://res.cloudinary.com/dwohsn29d/image/upload/v1696745238/Portfolio/images/o1ltqgklgsxdhnvh1loe.png",
         "library": "cg",
-    "style": "shadow-blue-700",
+    "style": "shadow-blue-800",
+    "bg":"rgb(18,64,111)",
  "content":"C"
   },
  {
     "id": 8,
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745238/Portfolio/images/d60dn8wevamsjysoq6dy.png",
      "library": "cg",
-    "style": "shadow-sky-400",
+    "style": "shadow-sky-500",
+    "bg":"rgba(14, 165, 213) ",
     "content":"Cpp"
   },
  {
@@ -71,13 +80,15 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745241/Portfolio/images/wx99qa9iblj2inockiyb.png",
      "library": "cg",
     "style": "shadow-lime-500",
+    "bg":"rgb(0, 255, 0)",
     "content":"NodeJs"
   },
  {
     "id": 10,
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745239/Portfolio/images/dea2aqakflnxxzzywapu.png",
      "library": "cg",
-    "style": "shadow-gray-500 pt-8",
+    "style": "shadow-gray-500 pt-8 invert",
+    "bg":"rgba(255, 255, 255, 0.9)",
     "content":"ExpressJs"
   },
  {
@@ -85,6 +96,7 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745240/Portfolio/images/sqwnwprtxhcfaerhgolj.png",
      "library": "cg",
     "style": "shadow-lime-500",
+    "bg":"rgb(0, 255, 0)",
     "content":"MongoDb"
   },
  
@@ -92,28 +104,32 @@ export const iconsData: Array<IconsTypes> = [
     "id": 12,
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745242/Portfolio/images/s15x18ibpjjp3ja9aqcc.png",
      "library": "cg",
-    "style": "shadow-sky-400",
+    "style": "shadow-sky-500",
+    "bg":"rgba(14, 165, 213) ",
     "content":"PostgreSql"
    },
  {
     "id": 13,
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745238/Portfolio/images/gl3r9byb6p9ow8rxug7d.webp",
      "library": "cg",
-    "style": "shadow-purple-400 pt-4",
+    "style": "shadow-purple-500 pt-4",
+    "bg":"rgb( 88, 62, 158)",
     "content":"Bootstrap"
    },
  {
     "id": 14,
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745240/Portfolio/images/vvinvhdfitchcqblwohd.png",
      "library": "cg",
-    "style": "shadow-sky-400 pt-4",
+    "style": "shadow-sky-500 pt-4",
+    "bg":"rgba(14, 165, 213) ",
     "content":"MySql"
    },
   {
     "id": 15,
-    "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745242/Portfolio/images/qagoeloeyesfai37vtit.png",
+    "src": "/styled.png",
      "library": "cg",
-     "style": "shadow-pink-400",
+     "style": "shadow-pink-400 ",
+     "bg":"rgb(228,0,124)",
     "content":"Styled-Component"
   },
   {
@@ -121,15 +137,34 @@ export const iconsData: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745237/Portfolio/images/axt704a3aml5iwapu43b.png",
     "library": "cg",
      "style": "shadow-red-500 from-blue-500 to-red-500",
+     "bg":"rgb(239, 68, 68, 0.8)",
      "content":"Ant-Design"
    
   },
   {
     "id": 17,
-    "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745240/Portfolio/images/ho7pihnvhrgethophjqj.png",
+    "src": "/md.png",
     "library": "cg",
-     "style": "shadow-gray-700 pt-5",
+     "style": "shadow-gray-700 pt-5 ",
+     "bg":"rgba(55, 65, 81)",
    "content":"MarkDown"
+  },
+  
+  {
+    "id": 18,
+    "src": "/redux-original.svg",
+    "library": "cg",
+    "style": "shadow-purple-400 ",
+    "bg":"rgb( 88, 62, 158)",
+   "content":"Sass"
+  },
+  {
+    "id": 19,
+    "src": "/sass.svg",
+    "library": "cg",
+    "style": "shadow-pink-400 pt-4",
+     "bg":"rgb(228,0,124)",
+   "content":"Sass"
   },
 
 
@@ -141,6 +176,7 @@ export const tools: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745243/Portfolio/images/snor0b0u3xpv6eftkx3w.png",
     "library": "cg",
       "style": "shadow-blue-500",
+      "bg":"rgba(14, 165, 223)",
 "content":"VS Code"
   },
  {
@@ -148,6 +184,7 @@ export const tools: Array<IconsTypes> = [
      "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745239/Portfolio/images/gbbdp44j9lesvas9qq48.png",
     "library": "cg",
     "style": "shadow-orange-700",
+    "bg":"rgba(255, 105, 0)",
 "content":"Git"
  
   },
@@ -156,6 +193,7 @@ export const tools: Array<IconsTypes> = [
       "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745239/Portfolio/images/m1rgyzff74faamtw6fcm.png",
     "library": "cg",
 "style":"shadow-white",
+"bg":"rgba(255, 255, 255, 0.9)",
 "content":"GitHub"
   },
  {
@@ -163,6 +201,7 @@ export const tools: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745242/Portfolio/images/lceqqybgvgd7ziv44rvo.webp",
     "library": "cg",
     "style": "shadow-orange-500",
+    "bg":"rgba(255, 165, 0)",
 "content":"Postman"
   },
  
@@ -171,6 +210,7 @@ export const tools: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745243/Portfolio/images/z2izcqwqrv7opwxpxkro.png",
     "library": "cg",
     "style": "shadow-gray-300 pt-10",
+    "bg":"rgba(255, 255, 255, 0.9)",
    "content":"Vercel"
   },
  {
@@ -178,6 +218,7 @@ export const tools: Array<IconsTypes> = [
     "src": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745238/Portfolio/images/casxkkl2asrvgu2wdcyg.png",
     "library": "cg",
     "style": "shadow-orange-400 pt-5",
+    "bg":"rgba(255, 165, 50)",
    "content":"AWS"
   }
 ]
