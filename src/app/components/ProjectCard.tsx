de58fa6c080deac4c126bdc7ed235c1bfc38634c
Osama <shaikhosama499@gmail.com>
2024-06-25 01:35:16 +0530

    chore(utils): new projects added

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FaEye } from "react-icons/fa";
 interface Project {
   id: number;
   name: string;
@@ -18,55 +19,55 @@ interface ProjectCardProps {
   project: Project;
 }
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
- 
-    return (
-      <>
-        <div className='w-[98%] my-5 mx-2 h-[1px] bg-[#2b2e33]'></div>
-        {/* ------------------------------------------------------------------ */}
-        <div className='py-[6px] mx-2 w-[98%] flex flex-col '> 
-          <div className="grid gap-5 grid-cols-1 midp:grid-cols-3 ">
-            <div className='flex flex-col gap-y-2 col-span-1 midp:col-span-2 order-2 midp:order-1'>
-              <span className='flex items-center gap-2'><a href={project.source_code}><h3 className='text-[#2f81f7] font-sans font-semibold text-xl cursor-pointer hover:underline'>{ project.name}</h3></a>
-                <span className='border border-[#2b2e33] rounded-2xl px-2 py-[1px] font-sans text-[#7d8590] text-xs font-semibold'>{ project.visibility}</span>
-              </span>
-              <div className='font-mono text-[#7d8590] '>
-            {project.description}
+
+  return (
+    <>
+      <div className='w-[98%] my-5 mx-2 h-[1px] bg-[#2b2e33]'></div>
+      {/* ------------------------------------------------------------------ */}
+      <div className='py-[6px] mx-2 w-[98%] flex flex-col '>
+        <div className="grid gap-5 grid-cols-1 midp:grid-cols-3 ">
+          <div className='flex flex-col gap-y-2 col-span-1 midp:col-span-2 order-2 midp:order-1'>
+            <span className='flex items-center gap-2'><a href={project.source_code}><h3 className='text-[#2f81f7] font-sans font-semibold text-xl cursor-pointer hover:underline'>{project.name}</h3></a>
+              <span className='border border-[#2b2e33] rounded-2xl px-2 py-[1px] font-sans text-[#7d8590] text-xs font-semibold'>{project.visibility}</span>
+            </span>
+            <div className='font-mono text-[#7d8590] '>
+              {project.description}
+            </div>
+            <div className='tag py-1 flex flex-wrap gap-[2px]'>
+              {project.tags.map((tag: any) => (
+
+                <span key={tag} className='cursor-pointer rounded-2xl  px-3 py-[3px] text-blue-600 font-sans bg-[#2f82f72b] text-sm font-semibold hover:bg-[#2f81f7] hover:text-[#e6edf3]'>
+                  {tag}
+                </span>
+
+              ))}
+
+            </div>
+
+            <div className='flex text-[#7d8590] text-sm items-center gap-[2px]'>
+              <span className={`h-3 w-3  rounded-full ${project.language}`}></span>{project.language}
+            </div>
           </div>
-          <div className='tag py-1 flex flex-wrap gap-[2px]'>
-                 {project.tags.map((tag:any) => (
-                   
-              <span key={tag} className='cursor-pointer rounded-2xl  px-3 py-[3px] text-blue-600 font-sans bg-[#2f82f72b] text-sm font-semibold hover:bg-[#2f81f7] hover:text-[#e6edf3]'>
-              {tag}
-              </span>
-        
-          ))}
-                
-          </div>
-          
-          <div className='flex text-[#7d8590] text-sm items-center gap-[2px]'>
-            <span className={`h-3 w-3  rounded-full ${project.language}`}></span>{project.language}
-              </div>
-              </div>
-            <div className=' flex flex-col items-center midp:items-end  justify-center order-1 midp:order-2'>
+          <div className=' flex flex-col items-center midp:items-end  justify-center order-1 midp:order-2'>
             <div className="relative group">
-      <Image
-                            className="select-none object-cover"
-                            src={project.image}
-                            width={260}
-                            height={260}
+              <Image
+                className="select-none object-cover"
+                src={project.image}
+                width={260}
+                height={260}
                 alt="profile" />
-        <Link href={project.live_link} target='_blank'>        <div className="overlay absolute left-8 right-[30px] top-4 bottom-6  inset-0 opacity-0 transition-opacity duration-400 ease-in-out bg-[#0d1117] cursor-pointer group-hover:opacity-80 group-hover:rounded">
-                  
-    <div className="content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#e6edf3] font-lora text-center">
-      <h3>LIVE</h3>
-    </div>
-  </div></Link>
-</div>
+              <Link href={project.live_link} target='_blank'>        <div className="overlay absolute left-8 right-[30px] top-4 bottom-5  inset-0 opacity-0 transition-opacity duration-400 ease-in-out bg-[#0d1117] cursor-pointer group-hover:opacity-80 group-hover:rounded">
+
+                <div className="content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#e6edf3] font-lora text-center">
+                  <h3 className = "flex justify-center items-center col gap-2"><FaEye/>LIVE</h3>
+                </div>
+              </div></Link>
+            </div>
 
           </div>
-         </div>
         </div>
-        </>
+      </div>
+    </>
   )
 }
 
--- src/app/utils/projectData.ts
+++ src/app/utils/projectData.ts
@@ -1,4 +1,4 @@
-export interface ProjectDataTypes  {
+export interface ProjectDataTypes {
   id: number;
   name: string;
   image: string;
@@ -10,72 +10,106 @@ export interface ProjectDataTypes  {
   visibility: string;
 }
 export const projectData: Array<ProjectDataTypes> = [
-{
-"id":1,
-"name": "QuickClick",
-"image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745243/Portfolio/images/ujcjlcigsicrkegspz4g.png",
-"description": "A dynamic E-Commerce platform designed to provide users with an efficient and enjoyable online shopping experience.",
-"tags": ["reactjs", "nodejs", "mongodb", "styled-components", "expressjs","react-ecommerce"],
-"source_code": "https://github.com/Osamashaikh90/QuickClick",
-"live_link": "https://quick-click.vercel.app",
-"language":"JavaScript",
-"visibility":"Public"
+  {
+    "id": 1,
+    "name": "QuickClick",
+    // "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745243/Portfolio/images/ujcjlcigsicrkegspz4g.png",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1719259357/Portfolio/images/qpl61mrxafldwrvi759z.png",
+    "description": "A dynamic E-Commerce platform designed to provide users with an efficient and enjoyable online shopping experience.",
+    "tags": ["reactjs", "nodejs", "mongodb", "styled-components", "expressjs", "react-ecommerce"],
+    "source_code": "https://github.com/Osamashaikh90/QuickClick",
+    "live_link": "https://quick-click.vercel.app",
+    "language": "JavaScript",
+    "visibility": "Public"
+  },
+  {
+    "id": 2,
+    "name": "Youtube Sync",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1719253021/Portfolio/images/zoldputceehz64t3onzy.png",
+    "description": "A revolutionary YouTube alternative: Explore, search, and View your youtube subscriptions with ease, featuring multilingual support, theme switching, voice search, and advanced state management.",
+    "tags": ["reactjs", "Google Api", "reduxjs", "redux-toolkit", "RTK query", "Rapid Api","i18next"],
+    "source_code": "https://github.com/Osamashaikh90/Youtube-Sync",
+    "live_link": "https://youtube-clone-nu-silk.vercel.app",
+    "language": "JavaScript",
+    "visibility": "Public"
 
-},
-{
-"id":2,
-"name":"Freelanster",
-"image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745239/Portfolio/images/b6ebq6uoik04i7dl8l0m.png",
-"description": "A platform Where Jobs Meet Talent: Connecting Job Seekers and Employers, Making Job Hunting and Hiring Effortless.",
-  "tags": ["nodejs","javascript","mongodb","styled-components","reactjs","expressjs","mui"],
-"source_code": "https://github.com/Bhanu1776/Freelansters",
-"live_link": "https://freelansters-git-master-bhanu1776.vercel.app/",
-"language":"JavaScript",
-"visibility":"Public"
-},
-{
-"id":3,
-"name":"DevDiaries",
- "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745239/Portfolio/images/eoyber3scurf7vb7z867.png",
-    "description": "DevDiaries is a personal blog website that aims to share my thoughts, experiences through modern web dev practices",
-    "tags": ["nextjs", "TypeScript", "tailwind", "gray-matter", "blogs"],
+  },
+  {
+    "id": 3,
+    "name": "Freelanster",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745239/Portfolio/images/b6ebq6uoik04i7dl8l0m.png",
+    "description": "A platform Where Jobs Meet Talent: Connecting Job Seekers and Employers, Making Job Hunting and Hiring Effortless.",
+    "tags": ["nodejs", "javascript", "mongodb", "styled-components", "reactjs", "expressjs", "mui"],
+    "source_code": "https://github.com/Bhanu1776/Freelansters",
+    "live_link": "https://freelansters-git-master-bhanu1776.vercel.app/",
+    "language": "JavaScript",
+    "visibility": "Public"
+  },
+  {
+    "id": 4,
+    "name": "DevDiaries",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745239/Portfolio/images/eoyber3scurf7vb7z867.png",
+    "description": "DevDiaries is a personal blog website that aims to share my thoughts, experiences through modern web dev practices.",
+    "tags": ["nextjs", "typeScript", "tailwind", "gray-matter", "blogs"],
     "source_code": "https://github.com/Osamashaikh90/DevDiaries",
-"live_link": "https://dev-diaries-kappa.vercel.app/",
-"language":"TypeScript",
-"visibility":"Public"
-},
-{
-"id":4,
-"name":"Task-tracker",
- "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745243/Portfolio/images/nulqesdekh8mr5wf49aw.png",
+    "live_link": "https://dev-diaries-kappa.vercel.app/",
+    "language": "TypeScript",
+    "visibility": "Public"
+  },
+  {
+    "id": 5,
+    "name": "Course Wallah",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1718290164/Portfolio/images/qzwult6onryhep2rzbuz.png",
+    "description": "Explore an innovative Ed-Tech platform where you can seamlessly add courses to your personalized dashboard, with real-time course likes updation feature.(Internship Assignment)",
+    "tags": ["Reactjs", "firebase", "tailwind", "redux-toolkit", "react-icons","vite"],
+    "source_code": "https://github.com/Osamashaikh90/alemeno-dashboard",
+    "live_link": "https://alemeno-dashboard.vercel.app/",
+    "language": "JavaScript",
+    "visibility": "Public"
+  },
+  {
+    "id": 6,
+    "name": "Task-tracker",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745243/Portfolio/images/nulqesdekh8mr5wf49aw.png",
     "description": "TaskTrackr is a todo-list app using NextJs with TS and Employed PostgreSQL as the database solution.",
-    "tags": ["nodejs"," typescript", "nextjs" ,"postgresql", "expressjs"," tailwindcss","to-do"],
+    "tags": ["nextjs", "nodejs", " typescript", "postgresql", "expressjs", " tailwindcss", "uid","dotenv"],
     "source_code": "https://github.com/Osamashaikh90/tasktrackr",
-"live_link": "https://tasktrackr-indol.vercel.app/",
-"language":"TypeScript",
-"visibility":"Public"
-},
-// {
-// "id":5,
-// "name":"Portfolio",
-//  "image": "/images/Profile.png",
-//     "description": "A platform for web developers to showcase their projects",
-//     "tags": ["next", "node", "mongodb", "tailwind", "react-query"],
-//     "source_code": "https://github.com/itsnitinr/driwwwle-v2",
-// "live_link": "https://driwwwle.com/",
-// "language":"TypeScript ",
-// "visibility":"Public"
-// },
-{
-"id":5,
-"name":"Blood-bank Management",
- "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745238/Portfolio/images/xe991fjoixuv04tlpndl.png",
+    "live_link": "https://tasktrackr-indol.vercel.app/",
+    "language": "TypeScript",
+    "visibility": "Public"
+  },
+  {
+    "id": 7,
+    "name": "Sticky Wall",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1718302194/Portfolio/images/ra2e3ov8azyzxpvy8jzn.png",
+    "description": "Sticky Wall is your ultimate space to create, search, view, edit, and delete to-do lists or notes with current date, keeping you organized and on track effortlessly.(InternShip Assignment)",
+    "tags": ["reactjs", "react-toastify", "tailwind", "reduxjs/toolkit","local-storage" ,"notes"],
+    "source_code": "https://github.com/Osamashaikh90/StickyWall",
+    "live_link": "https://sticky-wall-gilt.vercel.app/",
+    "language": "TypeScript",
+    "visibility": "Public"
+  },
+  {
+    "id": 8,
+    "name": "QuizApp",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1718300223/Portfolio/images/lyjzbwln6eckirhhqzvs.png",
+    "description": "QuizApp is the perfect platform to test your web development skills and refresh your knowledge through engaging quizzes.(Internship Assignment)",
+    "tags": ["reactjs", "react-hooks", "tailwind", "web", "quizes"],
+    "source_code": "https://github.com/Osamashaikh90/DevDiaries",
+    "live_link": "https://quiz-app-rust-psi.vercel.app/",
+    "language": "TypeScript",
+    "visibility": "Public"
+  },
+  {
+    "id": 9,
+    "name": "Blood-bank Management",
+    "image": "https://res.cloudinary.com/dwohsn29d/image/upload/v1696745238/Portfolio/images/xe991fjoixuv04tlpndl.png",
     "description": "A Desktop application for bloodbank using Swing,Awt and MySql database",
-    "tags": ["github", "java","mySql", 'swing', "awt","java-project","swing-gui"],
+    "tags": ["github", "java", "mySql", 'swing', "awt", "java-project", "swing-gui"],
     "source_code": "https://github.com/Osamashaikh90/Blood-bank_Management_Sysytem",
-"live_link": "https://github.com/Osamashaikh90/Blood-bank_Management_Sysytem",
-"language":"Java",
-"visibility":"Private"
-}
+    "live_link": "https://github.com/Osamashaikh90/Blood-bank_Management_Sysytem",
+    "language": "Java",
+    "visibility": "Private"
+  }
 ]
 
